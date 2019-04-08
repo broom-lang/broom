b3eeb7141e94b74eb2e95b4afeedc9610929475a
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-04-08 17:17:47 +0300

    Change TreeForeset example to actually exhibit double vision.

--- example/TreeForest.ml
+++ example/TreeForest.ml
@@ -1,23 +1,32 @@
 structure Tree :> sig
     type 'a tree
 
-    val map: ('a -> 'b) -> 'a tree -> 'b tree
+    val singleton: 'a -> 'a tree
+    val push: 'a tree -> 'a -> 'a tree
 end = struct
     datatype 'a tree = Branch of 'a Forest.forest
                      | Leaf of 'a
 
-    fun map f =
-        fn Branch forest => Branch (Forest.map f forest)
-         | Leaf v => Leaf (f v)
+    val singleton = Leaf
+
+    fun push tree v =
+        let val vtree = singleton v
+        in case tree
+           of Branch forest => Forest.push forest vtree
+            | Leaf _ => Forest.push (Forest.push Forest.empty tree) vtree
+        end
 end
 
 structure Forest :> sig
     type 'a forest
 
-    val map: ('a -> 'b) -> 'a forest -> 'b forest
+    val empty: 'a forest
+    val push: 'a forest -> 'a tree -> 'a forest
 end = struct
     type 'a forest = 'a Tree.tree list
 
-    fun map f trees = List.map (Tree.map f) trees
+    val empty = []
+
+    fun push forest tree = tree :: forest
 end
 
