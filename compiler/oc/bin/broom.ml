2c9bf5f56646d243b406d4a65560ffba41be40d4
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-07-26 21:33:30 +0300

    Add `eval` subcommand.

--- compiler/oc/bin/broom.ml
+++ compiler/oc/bin/broom.ml
@@ -7,24 +7,35 @@ let name = "broom"
 let name_c = String.capitalize_ascii name
 let prompt = name ^ "> "
 
+let rep input =
+    try
+        let stmts = Parse.parse_string_exn input in
+        let doc = PPrint.group (PPrint.separate_map (PPrint.semi ^^ PPrint.break 1) Ast.Term.stmt_to_doc
+            (Vector.to_list stmts)) in
+        PPrint.ToChannel.pretty 1.0 80 stdout doc;
+        print_newline ()
+    with
+        | SedlexMenhir.ParseError err ->
+            prerr_endline (SedlexMenhir.string_of_ParseError err)
+
 let repl () =
     let rec loop () =
         match LNoise.linenoise prompt with
         | None -> ()
         | Some input ->
             let _ = LNoise.history_add input in
-            (try
-                let stmts = Parse.parse_string_exn input in
-                let doc = PPrint.group (PPrint.separate_map (PPrint.semi ^^ PPrint.break 1) Ast.Term.stmt_to_doc
-                    (Vector.to_list stmts)) in
-                PPrint.ToChannel.pretty 1.0 80 stdout doc;
-                print_newline ()
-            with
-                | SedlexMenhir.ParseError err ->
-                    prerr_endline (SedlexMenhir.string_of_ParseError err));
+            rep input;
             loop () in
     print_endline (name_c ^ " prototype REPL. Press Ctrl+D (on *nix, Ctrl+Z on Windows) to quit.");
     loop ()
+
+let eval_t =
+    let doc = "evaluate statements" in
+    let expr =
+        let docv = "STATEMENTS" in
+        let doc = "the statements to evaluate" in
+        C.Arg.(value & pos 0 string "" & info [] ~docv ~doc) in
+    (C.Term.(const rep $ expr), C.Term.info "eval" ~doc)
 
 let repl_t =
     let doc = "interactive evaluation loop" in
@@ -36,5 +47,5 @@ let default_t =
 
 let () =
     Hashtbl.randomize ();
-    C.Term.exit (C.Term.eval_choice default_t [repl_t])
+    C.Term.exit (C.Term.eval_choice default_t [repl_t; eval_t])
 
