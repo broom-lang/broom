362ad3dea5158083e8756bfaa4646f21d493398c
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-08-19 21:36:22 +0300

    Type Proxy exprs.

--- compiler/oc/lib/Typing.ml
+++ compiler/oc/lib/Typing.ml
@@ -13,8 +13,13 @@ let const_typ c = T.Prim (match c with
     | Const.Int _ -> Prim.Int)
 
 let typeof env (expr : AExpr.t with_pos) : FExpr.t with_pos typing = match expr.v with
+    | AExpr.Proxy typ ->
+        let typ = E.elaborate env {v = typ; pos = expr.pos} in
+        {term = {expr with v = Proxy typ}; typ = Type typ; eff = Prim EmptyRow}
+
     | AExpr.Use name ->
         {term = {expr with v = Use name}; typ = Env.find name env; eff = Prim EmptyRow}
+
     | AExpr.Const c -> {term = {expr with v = Const c}; typ = const_typ c; eff = Prim EmptyRow}
 
 let check_pat env typ (pat : AExpr.pat with_pos) : FExpr.lvalue * Env.t = match pat.v with
@@ -27,6 +32,7 @@ let check_stmt env : AStmt.t -> FStmt.t typing * Env.t = function
         let {term = expr; typ; eff} : FExpr.t with_pos typing = typeof env expr in
         let (pat, env) = check_pat env typ pat in
         ({term = FStmt.Def (pos, pat, expr); typ; eff}, env)
+
     | AStmt.Expr expr ->
         let typing = typeof env expr in
         ({typing with term = FStmt.Expr typing.term}, env)
