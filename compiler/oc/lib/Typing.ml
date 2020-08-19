f554ceae1a0041cb7ea0f6f14bb2994f4715d9b5
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-08-19 21:41:38 +0300

    Typecheck type ascription.

--- compiler/oc/lib/Typing.ml
+++ compiler/oc/lib/Typing.ml
@@ -12,7 +12,12 @@ type 'a typing = 'a TyperSigs.typing
 let const_typ c = T.Prim (match c with
     | Const.Int _ -> Prim.Int)
 
-let typeof env (expr : AExpr.t with_pos) : FExpr.t with_pos typing = match expr.v with
+let rec typeof env (expr : AExpr.t with_pos) : FExpr.t with_pos typing = match expr.v with
+    | AExpr.Ann (expr, typ) ->
+        let typ = E.elaborate env typ in
+        (* FIXME: Abstract type generation effect: *)
+        check env typ expr
+
     | AExpr.Proxy typ ->
         let typ = E.elaborate env {v = typ; pos = expr.pos} in
         {term = {expr with v = Proxy typ}; typ = Type typ; eff = Prim EmptyRow}
@@ -21,6 +26,8 @@ let typeof env (expr : AExpr.t with_pos) : FExpr.t with_pos typing = match expr.
         {term = {expr with v = Use name}; typ = Env.find name env; eff = Prim EmptyRow}
 
     | AExpr.Const c -> {term = {expr with v = Const c}; typ = const_typ c; eff = Prim EmptyRow}
+
+and check env typ expr = failwith "TODO"
 
 let check_pat env typ (pat : AExpr.pat with_pos) : FExpr.lvalue * Env.t = match pat.v with
     | AExpr.Use name -> ({name; typ}, Env.add name typ env)
