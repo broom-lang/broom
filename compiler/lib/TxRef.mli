1af6ebcc7b5cd8caabe2f5564e5c6a50b33aad10
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-09-05 17:57:30 +0300

    Make TxRef polymorphic with Obj voodoo.

--- compiler/lib/TxRef.mli
+++ compiler/lib/TxRef.mli
@@ -1,32 +1,10 @@
-module type TRANSACTABLE = sig
-    type log
-    type contents
-    type ref
+type log
+type 'a ref
 
-    val ref : contents -> ref
-    val get : ref -> contents
-    val set : log -> ref -> contents -> unit
-end
+val log : unit -> log
+val transaction : log -> (unit -> 'a) -> 'a
 
-module type S = sig
-    type log
-
-    val log : unit -> log
-    val transaction : log -> (unit -> 'a) -> 'a
-
-    module Type : TRANSACTABLE
-    module Expr : TRANSACTABLE
-    module Uv : TRANSACTABLE
-end
-
-module type TRANSACTABLES = sig
-    type typ
-    type expr
-    type uvv
-end
-
-module Make (Transactables : TRANSACTABLES) : S
-    with type Type.contents = Transactables.typ
-    with type Expr.contents = Transactables.expr
-    with type Uv.contents = Transactables.uvv
+val ref : 'a -> 'a ref
+val get : 'a ref -> 'a
+val set : log -> 'a ref -> 'a -> unit
 
