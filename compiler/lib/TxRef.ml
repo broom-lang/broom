b38ee888e7adda2fde90282a53903f7465ee182e
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-09-05 21:16:40 +0300

    Reduce allocations in TxRef.set.

--- compiler/lib/TxRef.ml
+++ compiler/lib/TxRef.ml
@@ -1,32 +1,39 @@
 module Log = struct
-    type undo = Undo : 'a ref * 'a -> undo
-
-    (* OPTIMIZE: Struct of Arrays?: *)
     type t =
         { mutable nesting : int
-        ; undos : undo CCVector.vector }
+        ; refs : Obj.t ref CCVector.vector
+        ; vals : Obj.t CCVector.vector }
 
-    let log () = {nesting = 0; undos = CCVector.create ()}
+    let log () =
+        { nesting = 0
+        ; refs = CCVector.create ()
+        ; vals = CCVector.create () }
 
-    let record log ref v = CCVector.push log.undos (Undo (ref, v))
+    let record : t -> 'a ref -> unit = fun log ref ->
+        let ref = Obj.magic ref in
+        CCVector.push log.refs ref;
+        CCVector.push log.vals !ref
 
-    let memento log = CCVector.length log.undos
+    let memento log = CCVector.length log.refs
 
     let abort =
-        let rec loop undos memento i =
+        let rec loop refs vals memento i =
             if i >= memento
             then begin
-                let Undo (ref, v) = CCVector.get undos i in
+                let ref = CCVector.get refs i in
+                let v = CCVector.get vals i in
                 ref := v;
-                loop undos memento (i - 1)
+                loop refs vals memento (i - 1)
             end in
-        fun ({nesting = _; undos} as log) memento ->
-            loop undos memento (CCVector.length undos - 1);
-            CCVector.truncate undos memento;
+        fun ({nesting = _; refs; vals} as log) memento ->
+            loop refs vals memento (CCVector.length refs - 1);
+            CCVector.truncate refs memento;
+            CCVector.truncate vals memento;
             log.nesting <- log.nesting - 1
 
     let commit log memento =
-        CCVector.truncate log.undos memento;
+        CCVector.truncate log.refs memento;
+        CCVector.truncate log.vals memento;
         log.nesting <- log.nesting - 1
 
     let transaction log body =
@@ -57,7 +64,7 @@ let get log ref = (log, !ref)
 
 let set (log : Log.t) ref v =
     if log.nesting > 0
-    then Log.record log ref !ref;
+    then Log.record log ref;
     ref := v;
     log
 
