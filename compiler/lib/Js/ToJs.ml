5bc2dfc42b019f55a4b7f1e624d74a7bacb24879
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-10-17 17:52:15 +0300

    Emit JS for WIth and Match.

--- compiler/lib/Js/ToJs.ml
+++ compiler/lib/Js/ToJs.ml
@@ -30,7 +30,8 @@ let emit program =
         docs in
     let emit_param label i = Vector.get (Cont.Id.Hashtbl.find param_docs label) i in
 
-    let emit_label (label : Cont.Id.t) name =
+    let emit_label (label : Cont.Id.t) =
+        let {name; _} : Cont.t = Program.cont program label in
         let prefix = match name with
             | Some name -> Name.to_doc name
             | None -> string "fn" in
@@ -49,10 +50,11 @@ let emit program =
                 let {Expr.pos = _; typ = _; cont = parent'; term} = Program.expr program id in
                 (* assert (Cont.Id.equal (Option.get parent') parent); FIXME *)
                 let var = if usecount id > 1 then Some (define id) else None in
-                let expr = match term with (* OPTIMIZE: unbox tuples (in an earlier pass) *)
-                    | Values vals -> surround_separate_map 4 0 (brackets empty)
-                        lbracket (comma ^^ break 1) rbracket
-                        emit_expr (Vector.to_list vals)
+                let expr = match term with
+                    | Values vals -> (* OPTIMIZE: unbox tuples (in an earlier pass) *)
+                        surround_separate_map 4 0 (brackets empty)
+                            lbracket (comma ^^ break 1) rbracket
+                            emit_expr (Vector.to_list vals)
 
                     | Focus {focusee; index} -> (* OPTIMIZE: parens not always necessary *)
                         parens (emit_expr focusee) ^^ brackets (string (Int.to_string index))
@@ -78,15 +80,27 @@ let emit program =
                         ) fields;
                         base_name
 
+                    | With {base; label; field} ->
+                        let base_name = fresh () in
+                        let stmt = infix 4 1 equals
+                            (string "var" ^^ blank 1 ^^ base_name)
+                            (string "Object.assign"
+                            ^^ parens (braces empty ^^ comma ^^ break 1 ^^ emit_expr base)) in
+                        CCVector.push stmts stmt;
+                        let stmt = infix 4 1 equals (base_name ^^ dot ^^ Name.to_doc label)
+                            (emit_expr field) in
+                        CCVector.push stmts stmt;
+                        base_name
+
                     | Select {selectee; field} -> (* OPTIMIZE: parens not always necessary *)
                         prefix 4 0 (parens (emit_expr selectee)) (dot ^^ Name.to_doc field)
 
-                    | Proxy _ -> string "void 0" (* OPTIMIZE: empty unboxed tuple = erase *)
+                    | Proxy _ -> string "0" (* OPTIMIZE: empty unboxed tuple = erase *)
 
                     | Param {label; index} -> emit_param label index
                     | Label label ->
                         let cont = Program.cont program label in
-                        let name_doc = emit_label label cont.name in
+                        let name_doc = emit_label label in
                         emit_fn label name_doc cont
 
                     | Const c -> emit_const c in
@@ -98,20 +112,43 @@ let emit program =
                     var
                 | None -> expr) in
 
+        let emit_clause {Transfer.pat; dest} =
+            let emit_pat = function
+                | Pattern.Const c -> emit_const c
+                | Wild -> string "default" in
+
+            let {Cont.pos = _; name = _; universals = _; params; body} = Program.cont program dest in
+            string "case" ^^ blank 1 ^^ emit_pat pat ^^ colon ^^ blank 1
+            ^^ emit_block dest body ^^ semi in
+
         let transfer = match transfer.term with
-            | Jump {callee; universals = _; args} ->
-                parens (emit_expr callee) (* OPTIMIZE: don't always need parens *)
+            | Goto {callee; universals = _; args} ->
+                string "return" ^^ blank 1 ^^ parens (emit_label callee) (* OPTIMIZE: don't always need parens *)
                 ^^ surround_separate_map 4 0 (parens empty)
                     lparen (comma ^^ blank 1) rparen
                     emit_expr (Vector.to_list args)
+
+            | Jump {callee; universals = _; args} ->
+                string "return" ^^ blank 1 ^^ parens (emit_expr callee) (* OPTIMIZE: don't always need parens *)
+                ^^ surround_separate_map 4 0 (parens empty)
+                    lparen (comma ^^ blank 1) rparen
+                    emit_expr (Vector.to_list args)
+
+            | Match {matchee; clauses} ->
+                string "switch" ^^ blank 1 ^^ parens (emit_expr matchee) ^^ blank 1
+                ^^ surround_separate_map 0 0 (braces empty)
+                    lbrace (break 1 ^^ string "break" ^^ semi ^^ break 1) rbrace
+                    emit_clause (Vector.to_list clauses)
+
             | Return (_, args) ->
-                Stream.from (Vector.to_source args)
+                string "return" ^^ blank 1 ^^ (Stream.from (Vector.to_source args)
                     |> Stream.map emit_expr
                     |> Stream.into Sink.list
                     |> surround_separate 4 0 (brackets empty)
-                        lbracket (comma ^^ break 1) rbracket in
+                        lbracket (comma ^^ break 1) rbracket) in
+
         concat_map (fun stmt -> stmt ^^ semi ^^ break 1) (CCVector.to_list stmts)
-        ^^ string "return" ^^ blank 1 ^^ transfer ^^ semi
+        ^^ transfer ^^ semi
 
     and emit_block parent transfer =
         surround 4 1 lbrace (emit_transfer parent transfer) rbrace
@@ -125,7 +162,7 @@ let emit program =
 
     let emit_export label =
         let cont = Program.cont program label in
-        let name_doc = emit_label label cont.name in
+        let name_doc = emit_label label in
         prefix 4 1 (string "var" ^^ blank 1 ^^ name_doc ^^ blank 1 ^^ equals)
             (emit_fn label name_doc cont ^^ semi ^^ hardline) in
 
