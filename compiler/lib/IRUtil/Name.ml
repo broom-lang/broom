e7b79c5e28591df98344f52622e4e4731836bc20
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-11-05 17:12:46 +0200

    Optimize Name.

--- compiler/lib/IRUtil/Name.ml
+++ compiler/lib/IRUtil/Name.ml
@@ -1,34 +1,56 @@
-type t
-    = String of string
-    | Fresh of int
-
-let counter = ref 0
-
-let fresh () = 
-    let i = !counter in
-    counter := i + 1;
-    Fresh i
-
-let of_string str = String str
-
-let compare = compare
-
-let to_string = function
-    | String s -> s
-    | Fresh n ->"__" ^ Int.to_string n
-
-let to_doc name = PPrint.string (to_string name)
-
-type name = t
-
-module Map = Map.Make(struct
-    type t = name
-    let compare = compare
-end)
-
-module Hashtbl = Hashtbl.Make(struct
-    type t = name
-    let equal = (=)
+module Cache = Hashtbl.Make(struct
+    type t = string
+    let equal = String.equal
     let hash = Hashtbl.hash
 end)
+
+module Key = struct
+    type t = int
+
+    let compare = Int.compare
+    let equal = Int.equal
+    let hash = Hashtbl.hash
+end
+
+module Hashtbl = Hashtbl.Make(Key)
+module Map = Map.Make(Key)
+
+include Key
+
+let fresh =
+    let counter = ref 0 in
+    fun () ->
+        let i = !counter in
+        counter := i + 1;
+        i
+
+let names = Hashtbl.create 0
+
+let of_string =
+    let cache = Cache.create 0 in
+    fun s ->
+        match Cache.find_opt cache s with
+        | Some n -> n
+        | None ->
+            let n = fresh () in
+            Hashtbl.add names n s;
+            Cache.add cache s n;
+            n
+
+let basename n = Hashtbl.find_opt names n
+
+let freshen n =
+    let n' = fresh () in
+    (match basename n with
+    | Some s -> Hashtbl.add names n' s
+    | None -> ());
+    n'
+
+let to_string n = 
+    let prefix = match basename n with
+        | Some s -> s ^ "$"
+        | None -> "$" in
+    prefix ^ Int.to_string n
+
+let to_doc n = PPrint.string (to_string n)
 
--- compiler/lib/IRUtil/Name.mli
+++ compiler/lib/IRUtil/Name.mli
@@ -1,9 +1,16 @@
 type t
 
-val of_string : string -> t
-val to_string : t -> string
+val equal : t -> t -> bool
 val compare : t -> t -> int
+val hash : t -> int
+
 val fresh : unit -> t
+val of_string : string -> t
+val freshen : t -> t
+
+val basename : t -> string option
+
+val to_string : t -> string
 val to_doc : t -> PPrint.document
 
 module Map : Map.S with type key = t
