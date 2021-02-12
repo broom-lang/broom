8e26bd6d02dc4f30a7fc36a0078fb6372d51d43c
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-02-13 00:37:06 +0200

    Remove unused TxRef parts

--- compiler/lib/Typechecker/TxRef.ml
+++ compiler/lib/Typechecker/TxRef.ml
@@ -8,10 +8,6 @@ end
 
 type 'a t = Id.t
 
-let equal r r' = Id.equal (Obj.magic r) (Obj.magic r')
-
-let hash r = Id.hash (Obj.magic r)
-
 let ref v =
     let id = Id.fresh () in
     Subst.current := Id.HashMap.add id (Obj.repr v) !Subst.current;
@@ -20,39 +16,5 @@ let ref v =
 let (!) (id : Id.t) = Obj.obj (Id.HashMap.get_exn (Obj.magic id) !Subst.current)
 
 let (:=) (id : Id.t) v =
-    Subst.current := Id.HashMap.add (Obj.magic id) (Obj.repr v) Stdlib.(!Subst.current)
-
-type 'a txref = 'a t
-
-module type T = sig type t end
-
-module Hashtbl = struct
-    module Make (T : T) = CCHashtbl.Make (struct
-        type t = T.t txref
-
-        let equal = equal
-        let hash = hash
-    end)
-end
-
-module HashSet = struct
-    module Make (T : T) = CCHashSet.Make (struct
-        type t = T.t txref
-
-        let equal = equal
-        let hash = hash
-    end)
-end
-
-module Set = struct
-    type 'a t = Id.Set.t
-
-    let empty = Id.Set.empty
-    let add v set = Id.Set.add (Obj.magic v) set
-    let remove v set = Id.Set.remove (Obj.magic v) set
-    let is_empty = Id.Set.is_empty
-    let mem v set = Id.Set.mem (Obj.magic v) set
-
-    let iter = Id.Set.iter
-end
+    Stdlib.(Subst.current := Id.HashMap.add (Obj.magic id) (Obj.repr v) !Subst.current)
 
--- compiler/lib/Typechecker/TxRef.mli
+++ compiler/lib/Typechecker/TxRef.mli
@@ -4,33 +4,7 @@ end
 
 type 'a t
 
-val equal : 'a t -> 'a t -> bool
-
 val ref : 'a -> 'a t
 val (!) : 'a t -> 'a
 val (:=) : 'a t -> 'a -> unit
-
-type 'a txref = 'a t
-
-module type T = sig type t end
-
-module Hashtbl : sig
-    module Make (T : T) : CCHashtbl.S with type key = T.t t
-end
-
-module HashSet : sig
-    module Make (T : T) : CCHashSet.S with type elt = T.t t
-end
-
-module Set : sig
-    type 'a t
-
-    val empty : 'a t
-    val add : 'a txref -> 'a t -> 'a t
-    val remove : 'a txref -> 'a t -> 'a t
-    val is_empty : 'a t -> bool
-    val mem : 'a txref -> 'a t -> bool
-
-    val iter : ('a txref -> unit) -> 'a t -> unit
-end
 
