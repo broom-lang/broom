201f64529e634b1b84bc24c13dbdc833e85c1b52
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-04-28 10:56:34 +0300

    Add Util.with_prec

--- compiler/lib/Util/Util.ml
+++ compiler/lib/Util/Util.ml
@@ -1,3 +1,5 @@
+type document = PPrint.document
+
 type plicity = Explicit | Implicit
 
 let plicity_arrow = function
@@ -11,6 +13,27 @@ let pos_to_string ({pos_lnum = line; pos_bol = bol; pos_cnum = offset; pos_fname
 let span_to_string (pos, pos') = pos_to_string pos ^ "-" ^ pos_to_string pos'
 
 type 'a with_pos = {v : 'a; pos: span}
+
+type 'a docs_op =
+    | InfixL of {prec : int; l : 'a; op : document; r : 'a}
+    | InfixR of {prec : int; l : 'a; op : document; r : 'a}
+    | Unary of document
+
+let show_paren show doc = if show then PPrint.parens doc else doc
+
+let with_prec to_docs v =
+    let rec to_doc ctx_prec v =
+        match to_docs ctx_prec v with
+        | InfixL {prec; l; op; r} ->
+            PPrint.infix 4 1 op (to_doc prec l) (to_doc (prec + 1) r)
+            |> show_paren (ctx_prec > prec)
+
+        | InfixR {prec; l; op; r} ->
+            PPrint.infix 4 1 op (to_doc (prec + 1) l) (to_doc prec r)
+            |> show_paren (ctx_prec > prec)
+
+        | Unary doc -> doc in
+    to_doc 0 v
 
 let doc_to_string doc =
     let buf = Buffer.create 0 in
--- compiler/lib/Util/Util.mli
+++ compiler/lib/Util/Util.mli
@@ -1,6 +1,8 @@
+type document = PPrint.document
+
 type plicity = Explicit | Implicit
 
-val plicity_arrow : plicity -> PPrint.document
+val plicity_arrow : plicity -> document
 
 type span = Lexing.position * Lexing.position
 
@@ -8,5 +10,12 @@ val span_to_string : span -> string
 
 type 'a with_pos = {v : 'a; pos: span}
 
-val doc_to_string : PPrint.document -> string
+type 'a docs_op =
+    | InfixL of {prec : int; l : 'a; op : document; r : 'a}
+    | InfixR of {prec : int; l : 'a; op : document; r : 'a}
+    | Unary of document
+
+val with_prec : (int -> 'a -> 'a docs_op) -> 'a -> document
+
+val doc_to_string : document -> string
 
