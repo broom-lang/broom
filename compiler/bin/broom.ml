5194d69a6f365e7a8ce652f841de05bc2a2ab629
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-04-29 20:42:10 +0300

    Reinstate REPL

--- compiler/bin/broom.ml
+++ compiler/bin/broom.ml
@@ -4,8 +4,8 @@ module C = Cmdliner
 module PP = PPrint
 
 let name = "broom"
-(*let name_c = String.capitalize_ascii name
-let prompt = name ^ "> "*)
+let name_c = String.capitalize_ascii name
+let prompt = name ^ "> "
 
 let debug_heading str =
     print_endline ("\n" ^ str ^ "\n" ^ String.make (String.length str) '=' ^ "\n")
@@ -172,7 +172,7 @@ let rep debug envs filename input =
             flush stderr);
         envs
 
-(*let repl path debug =
+let repl path debug =
     let rec loop envs =
         match LNoise.linenoise prompt with
         | None -> ()
@@ -181,9 +181,9 @@ let rep debug envs filename input =
             let envs = rep debug envs "REPL input" (Sedlexing.Utf8.from_string input) in
             loop envs in
     print_endline (name_c ^ " prototype REPL. Press Ctrl+D (on *nix, Ctrl+Z on Windows) to quit.");
-    loop (Expander.Bindings.empty path, Typer.Env.interactive (), Fc.Eval.Namespace.create ())
+    loop (Expander.Bindings.empty path, Namespace.empty)
 
-let lep path debug filename =
+(*let lep path debug filename =
     let input = open_in filename in
     Fun.protect (fun () ->
         rep debug (eval_envs path) filename (Sedlexing.Utf8.from_channel input)
@@ -241,11 +241,11 @@ let eval_t =
         let doc = "the file to evaluate" in
         C.Arg.(value & pos 0 string "" & info [] ~docv ~doc) in
     ( C.Term.(const ignore $ (const lep $ path $ debug $ filename))
-    , C.Term.info "script" ~doc )
+    , C.Term.info "script" ~doc )*)
 
 let repl_t =
     let doc = "interactive evaluation loop" in
-    (C.Term.(const repl $ path $ debug), C.Term.info "repl" ~doc)*)
+    (C.Term.(const repl $ path $ debug), C.Term.info "repl" ~doc)
 
 let default_t =
     let doc = "effective, modular, functional programming language. WIP." in
@@ -255,5 +255,5 @@ let default_t =
 
 let () =
     Hashtbl.randomize ();
-    C.Term.exit (C.Term.eval_choice default_t [(*build_t; check_t; repl_t; script_t;*) eval_t])
+    C.Term.exit (C.Term.eval_choice default_t [(*build_t; check_t;*) repl_t; (*script_t;*) eval_t])
 
